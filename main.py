be4533e1eb1c89bf1932b8273005c9213bb8fd91
Brunomdd <brunnodiniiz@gmail.com>
2026-04-03 21:15:26 -0300

    feat: adiciona a função main com opções e input do usuário

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world")
+from pytubefix import YouTube
+
+def main():
+    while True:
+        print('1 - Converter')
+        print('2 - Sair')
+        opcao = int(input('escolha uma opção: '))
+        if opcao == 1:
+            url = input('URL: ').strip()
+        elif opcao == 2:
+            print("Saindo . . . ")
+            break
+        else:
+            print('Opção inválida')
+
+main()
