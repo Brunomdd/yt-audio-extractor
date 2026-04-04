0cddbc887da906c9561181488904e3fe68331237
Brunomdd <brunnodiniiz@gmail.com>
2026-04-03 22:13:37 -0300

    feat: adiciona a função linha e cabecalho

--- uteis.py
+++ uteis.py
@@ -1,3 +1,11 @@
+def linha(txt=32):
+    return '-'*txt
+
+def cabecalho(txt):
+    print(linha())
+    print(f'{txt}'.center(32))
+    print(linha())
+
 def leiaint(msg):
     while True:
         try:
