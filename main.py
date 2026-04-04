acfa6aba292f3b3ddb9dd0bf0147e256993156b0
Brunomdd <brunnodiniiz@gmail.com>
2026-04-03 21:31:22 -0300

    feat: adiciona a função baixar_audio de vídeos para áudio

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+from uteis import leiaint
 from pytubefix import YouTube
-def leiaint(msg):
-    while True:
-        try:
-            valor = int(input(msg))
-            return valor
-        except ValueError:
-            print('Erro, digite um número inteiro!')
+
+def baixar_audio(url):
+    try:
+        yt = YouTube(url)
+        audio = yt.streams.get_audio_only()
+        audio_final = audio.download()
+        return  audio_final
+    except Exception as error:
+        print(f"Erro {error}")
 
 def main():
     while True:
@@ -14,6 +17,8 @@ def main():
         opcao = leiaint('escolha uma opção: ')
         if opcao == 1:
             url = input('URL: ').strip()
+            arquivo = baixar_audio(url)
+            print(arquivo)
         elif opcao == 2:
             print("Saindo . . . ")
             break
