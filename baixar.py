8da54f9712bcad9c9ca1ee8727716b2e31eb6c1a
Brunomdd <brunnodiniiz@gmail.com>
2026-04-03 21:57:50 -0300

    feat: adiciona parâmetro destino com valor padrao '.' na função baixar_áudio

--- baixar.py
+++ baixar.py
@@ -1,10 +1,11 @@
 from pytubefix import YouTube
 
-def baixar_audio(url):
+def baixar_audio(url,destino='.'):
     try:
         yt = YouTube(url)
         audio = yt.streams.get_audio_only()
-        audio_final = audio.download()
+        audio_final = audio.download(output_path=destino)
         return  audio_final
     except Exception as error:
         print(f"Erro {error}")
+
