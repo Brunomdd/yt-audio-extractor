edf14c5124d2deca7ce4685eecb407d642e766ef
Brunomdd <brunnodiniiz@gmail.com>
2026-04-04 13:57:15 -0300

    refactor: padroniza tratamento de erros e retorna mensagens consistentes

--- baixar.py
+++ baixar.py
@@ -1,16 +1,19 @@
 from pytubefix import YouTube
+from pytubefix.exceptions import VideoRemovedByYouTubeForViolatingTOS, VideoUnavailable
 
 def baixar_audio(url,destino='.'):
     try:
         yt = YouTube(url)
-        print(f"Título: {yt.title}")
-        print(f"Quantidade de views {yt.views}")
-        print(f"Quantidade de likes: {yt.likes}")
-        print(f"Autor: {yt.author}")
         audio = yt.streams.get_audio_only()
         audio_final = audio.download(output_path=destino)
         return  audio_final
+
+    except VideoRemovedByYouTubeForViolatingTOS:
+        return "Video foi removido do youtube por violar os direitos autorais"
+    except VideoUnavailable:
+        return "O video está indisponivel!"
     except Exception as error:
-        print(f"Erro ao baixar o áudio {error}")
-        
+        return f"Erro ao baixar o áudio {error}"
+
+
 
