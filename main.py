c6289f3925227a22d5ff2257540e4ff830585080
Brunomdd <brunnodiniiz@gmail.com>
2026-04-03 21:21:54 -0300

    feat: adiciona função leiaint para tratamento de input do usuário

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from pytubefix import YouTube
+def leiaint(msg):
+    while True:
+        try:
+            valor = int(input(msg))
+            return valor
+        except ValueError:
+            print('Erro, digite um número inteiro!')
 
 def main():
     while True:
         print('1 - Converter')
         print('2 - Sair')
-        opcao = int(input('escolha uma opção: '))
+        opcao = leiaint('escolha uma opção: ')
         if opcao == 1:
             url = input('URL: ').strip()
         elif opcao == 2:
