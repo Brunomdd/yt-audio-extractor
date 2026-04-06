24ab7d34c165b9ec18ee2d616df7cacda8a81e06
Brunomdd <brunnodiniiz@gmail.com>
2026-04-06 11:32:36 -0300

    feat: adiciona cores para melhorar a UX do usuário

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
 from uteis import leiaint,cabecalho,linha,confirmar
 from baixar import  baixar_audio
 import validators
-from interface import interface
+from interface import interface,init,Fore,Style
+import os
+
 
 def main():
-    cabecalho("yt-audio-extractor")
+    cabecalho(Fore.CYAN +"yt-audio-extractor" + Style.RESET_ALL)
     while True:
         print('1 - Converter')
         print('2 - Sair')
         opcao = leiaint('escolha uma opção: ')
         if opcao == 1:
             url = input("URL: ").strip()
-            if not validators.url(url) or ('youtube.com' not in url and "youtu.be" not in url):
-                print(f'erro: URL inválida! use um link do Youtube!')
+            if not validators.url(url) or ("youtube.com" not in url and "youtu.be" not in url):
+                print(Fore.RED + f'erro: URL inválida! use um link do Youtube!'+ Style.RESET_ALL)
                 continue
-
             destino = input('DESTINO: ').strip()
+            os.makedirs(destino,exist_ok=True)
             interface(url)
             resp = confirmar("Quer baixar o arquivo? [S/N]: ")
             if resp == "S":
                 resposta = baixar_audio(url, destino)
-                if resposta['status'] == 'sucesso':
-                    print(f'Arquivo baixado com sucesso! Caminho: {resposta['caminho']}')
+                if resposta["status"] == 'sucesso':
+                    print(Fore.GREEN +f'o Arquivo foi baixado com sucesso! Caminho: {resposta["caminho"]}' + Style.RESET_ALL)
                 else:
-                    if resposta['status'] == 'erro':
-                        print(f'não foi possível baixar: {resposta['mensagem']}')
+                    print(Fore.RED+ f'não foi possível baixar: {resposta["mensagem"]}')
             else:
                 print("Download cancelado")
-
         elif opcao == 2:
-            cabecalho('Saindo do programa . . .')
+            cabecalho(Fore.LIGHTYELLOW_EX + 'Saindo do programa . . .'+ Style.RESET_ALL)
             break
         else:
-            print('Opção inválida')
+            print(Fore.RED + 'Opção inválida'+Style.RESET_ALL)
 
 if __name__=='__main__':
     main()
