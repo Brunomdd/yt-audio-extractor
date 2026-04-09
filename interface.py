206f6be32cd1d850d3c52f8341aaa5f8032fdd0d
Brunomdd <brunnodiniiz@gmail.com>
2026-04-09 11:23:11 -0300

    feat: adiciona minutos e segundos de duração corretamente

--- interface.py
+++ interface.py
@@ -1,15 +1,17 @@
-from pytubefix import YouTube
-from uteis import linha
-from colorama import init,Fore,Style
 
-def interface(url):
-        init()
-        yt = YouTube(url)
+from uteis import linha
+def interface(yt):
+    
         print(linha())
         print(f"Nome: {yt.title}")
+        minutos = yt.length // 60
+        segundos = yt.length % 60
         print(f"Quantidade de likes: {yt.likes}")
-        print(f"f Duração: {yt.length/60}")
+        print(f"f Duração: {minutos} e {segundos}")
         print(linha())
+
+
+
 
 
 
